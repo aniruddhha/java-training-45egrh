9cb77820c7a8b157d4ba6768f800f8213c415c27
aniruddhha <aniruddha.kudalkar@gmail.com>
2024-11-14 13:41:07 +0530

    simple method demo

---
+++ src/Methods.java
@@ -0,0 +1,11 @@
+public class Methods {
+
+    public static int addNumbers(int n1, int n2) {
+        return n1 + n2;
+    }
+
+    public static void main(String[] args) {
+        int sum = addNumbers(19,56);
+
+    }
+}
