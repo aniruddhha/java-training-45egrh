991cf1e792585df9baaded67ebef9320f6d2c79f
aniruddhha <aniruddha.kudalkar@gmail.com>
2024-11-14 13:58:06 +0530

    call by ref, call by value

--- src/Methods.java
+++ src/Methods.java
@@ -1,11 +1,24 @@
 public class Methods {
 
     public static int addNumbers(int n1, int n2) {
+        n1 = 300;
+
         return n1 + n2;
+    }
+
+    public static void copyArr(int[] arr) {
+        arr[0] = 90;
     }
 
     public static void main(String[] args) {
         int sum = addNumbers(19,56);
 
+        int n1 = 10;
+        int n2 = 30;
+
+        addNumbers(n1, n2);
+
+        int[] arr1 = { 1, 2, 4 };
+        copyArr(arr1);
     }
 }
