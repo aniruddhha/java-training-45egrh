d2a9168f54603d89ac6437c6ec87e3ffe4b44d63
aniruddhha <aniruddha.kudalkar@gmail.com>
2024-11-15 11:42:33 +0530

    classes and objects

---
+++ src/Car.java
@@ -0,0 +1,20 @@
+import java.util.Date;
+
+public class Car {
+
+    // state
+    int engineStatus = 0;
+    int lights;
+    String color;
+    Date mfg;
+    boolean isEv;
+
+    // behaviour
+    void start() { }
+
+    void stop() { }
+
+    void turnOnLights() {}
+
+    void turnOffLights() {}
+}
